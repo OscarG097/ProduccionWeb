ba35f64a5c3d9fc0a5326ce9b11f5aad9a640669
Oscar Gimenez <oscar010797@gmail.com>
2020-09-21 23:15:18 -0300

    Se arreglan categorias y productos del archivo .sql

--- glob.sql
+++ glob.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-09-2020 a las 01:09:40
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.3
+-- Tiempo de generación: 22-09-2020 a las 04:14:07
+-- Versión del servidor: 10.4.13-MariaDB
+-- Versión de PHP: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,51 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `glob`
 --
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `categorias`
---
-
-CREATE TABLE `categorias` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(50) COLLATE utf8mb4_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
---
--- Volcado de datos para la tabla `categorias`
---
-
-INSERT INTO `categorias` (`id`, `nombre`) VALUES
-(1, 'Calzado'),
-(2, 'Camperas'),
-(3, 'Camisetas'),
-(4, 'Short'),
-(5, 'Buzos'),
-(6, 'Accesorios');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `marcas`
---
-
-CREATE TABLE `marcas` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(50) COLLATE utf8mb4_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
---
--- Volcado de datos para la tabla `marcas`
---
-
-INSERT INTO `marcas` (`id`, `nombre`) VALUES
-(1, 'Adidas'),
-(2, 'Nike'),
-(3, 'Reebok'),
-(4, 'Topper'),
-(5, 'Everlast');
 
 -- --------------------------------------------------------
 
@@ -93,12 +47,12 @@ INSERT INTO `productos` (`id`, `marca_id`, `categoria_id`, `nombre`, `modelo`, `
 (2, 1, 1, 'Botin Adidas', 'Predator 19.4', 30, 1500, 0, 3),
 (3, 2, 6, 'Guantes Nike', 'GK Spyne Pro', 30, 1000, 0, 5),
 (4, 1, 3, 'Camiseta Adidas', 'Juventus \'20', 30, 1500, 0, 5),
-(5, 4, 2, 'Botin Nike', 'Mercuriarl Superfly', 30, 100, 0, 5),
+(5, 2, 1, 'Botin Nike', 'Mercurial Superfly', 30, 100, 0, 5),
 (6, 1, 2, 'Campera Adidas', 'Bayern Munich \'20', 30, 100, 0, 4),
-(7, 2, 4, 'Canillera Nike', 'Mercurial Lite', 30, 1000, 0, 5),
-(8, 2, 2, 'Camiseta Nike', 'Barcelona \'20', 30, 1000, 0, 5),
-(9, 1, 4, 'Pelota Adidas', 'UCL Final \'20', 30, 1000, 0, 5),
-(10, 1, 4, 'Guantes Adidas', 'BearBox Y', 30, 1000, 0, 5),
+(7, 2, 6, 'Canillera Nike', 'Mercurial Lite', 30, 1000, 0, 5),
+(8, 2, 3, 'Camiseta Nike', 'Barcelona \'20', 30, 1000, 0, 5),
+(9, 1, 6, 'Pelota Adidas', 'UCL Final \'20', 30, 1000, 0, 5),
+(10, 1, 6, 'Guantes Adidas', 'BearBox Y', 30, 1000, 0, 5),
 (11, 3, 1, 'Botas Boxeo Reebok', 'Master', 30, 1000, 0, 4),
 (12, 1, 1, 'Calzado Adidas', 'Hockey Narma', 30, 30, 0, 4),
 (13, 2, 2, 'Campera Nike', 'WindRunner', 30, 35, 0, 4),
@@ -115,18 +69,6 @@ INSERT INTO `productos` (`id`, `marca_id`, `categoria_id`, `nombre`, `modelo`, `
 --
 
 --
--- Indices de la tabla `categorias`
---
-ALTER TABLE `categorias`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `marcas`
---
-ALTER TABLE `marcas`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
@@ -135,18 +77,6 @@ ALTER TABLE `productos`
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
-
---
--- AUTO_INCREMENT de la tabla `categorias`
---
-ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT de la tabla `marcas`
---
-ALTER TABLE `marcas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`

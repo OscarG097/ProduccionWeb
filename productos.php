63eb2707f3be0466d7bc3697a3c538f1260679e8
Lucas Basualdo <basualdo1995@gmail.com>
2020-09-26 17:49:21 -0300

    Modificacion a productos.php para probar

--- productos.php
+++ productos.php
@@ -24,8 +24,9 @@
     </section>
     <!-- Breadcrumb Section End -->
 
-
-    <section class="product spad">
+    <section >
+    <br>
+    <br>
         <div class="container">
             <div class="row">
                 <div class="col-md-3 col-sm-5">
@@ -39,12 +40,15 @@
                         <div class="sidebar__item">
                             <h4>Rango Precio</h4>
                             <div class="price-range-wrap">
+
                                 <div class="price-range ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content"
                                     data-min="100" data-max="5000">
-                                    <div class="ui-slider-range ui-corner-all ui-widget-header"></div>
+                                    <div class="ui-slider-range ui-corner-all ui-widget-header">
+                                    </div>
                                     <span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default"></span>
                                     <span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default"></span>
-                                </div>
+                                    </div>
+
                                 <div class="range-slider">
                                     <div class="price-input">
                                         <input type="text" id="minamount">
@@ -56,20 +60,8 @@
                     </div>
                 </div>
 
-                <div class="col-lg-9 col-sm-7">
-
-                        <div class="row-fluid">
-                            <div class="col-md-12 col-sm-12">
-                                <div class="filter__found">
-                                    <h6><span><!--TOMAR CANTIDAD DE PRODUCTOS--></span> Productos Encontrados</h6>
-                                </div>
-                            </div>                      
-                        </div>
-
-                  
-                    <div class="col-md-3 col-sm-3">
+                <div class="col-md-3 col-sm-3">
                         <div class="row">
-                        <!--<div class="col-md-3 col-sm-7">-->
                                 <div class="product__item">
                                     <div class="product__item__text">
                                         <?php
@@ -84,12 +76,54 @@
                                                 <?php } ?>
                                      </div>
                                 </div>      
-                         <!--</div>-->
                         </div>
                     </div>
-                </div>   
-            </div>
+                    <div class="col-md-3 col-sm-3">
+                        <div class="row">
+                                <div class="product__item">
+                                    <div class="product__item__text">
+                                        <?php
+                                        foreach($Productos->getProductos($_GET) as $prod){
+                                        ?>
+                                              		
+                                        <a href="productos_detalle.php?prod=<?php echo $prod['id']?>"><img src="img/pagina_productos/<?php echo $prod['id']?>.jpg" alt=""></a>
+                                                <h4><?php echo $prod['nombre']?></h4>
+                                                <h5><?php echo $prod['modelo']?></h5>
+                                                <h7>$ <?php echo $prod['precio']?></strong></h7>
+										        <p>Stock: <?php echo $prod['cantidad']?></p>
+                                                <?php } ?>
+                                     </div>
+                                </div>      
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-3">
+                        <div class="row">
+                                <div class="product__item">
+                                    <div class="product__item__text">
+                                        <?php
+                                        foreach($Productos->getProductos($_GET) as $prod){
+                                        ?>
+                                              		
+                                        <a href="productos_detalle.php?prod=<?php echo $prod['id']?>"><img src="img/pagina_productos/<?php echo $prod['id']?>.jpg" alt=""></a>
+                                                <h4><?php echo $prod['nombre']?></h4>
+                                                <h5><?php echo $prod['modelo']?></h5>
+                                                <h7>$ <?php echo $prod['precio']?></strong></h7>
+										        <p>Stock: <?php echo $prod['cantidad']?></p>
+                                                <?php } ?>
+                                     </div>
+                                </div>      
+                        </div>
+                    </div>
+
+                    
+
+                 </div>
+        </div>
+
+                
     </section>
+
+    
     <!-- Product Section End -->
 
         <?php

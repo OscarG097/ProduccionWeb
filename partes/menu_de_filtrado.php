5778496566dc87d0d71dd9eec35b5948e41739ce
Luciano Labudia <luciano.a.labudia@gmail.com>
2020-09-26 13:55:29 -0300

    Ordenamiento actualizado al select

--- partes/menu_de_filtrado.php
+++ partes/menu_de_filtrado.php
@@ -43,16 +43,29 @@
 	<!-- Ordenamiento -->
 	<div class="filter__item">
         <div class="row-fluid">
-			<ul class="nav flex-column">				
+			<div class="filter__sort">				
 				
-				<h4>Ordenar Por</h4>
-
+				<!--
 				<li><a href="productos.php?order=AZ&marca=<?php echo (isset($_GET['marca'])?$_GET['marca']:'') ?>&cat=<?php echo isset($_GET['cat'])?$_GET['cat']:''?>"><span class="icon-chevron-right"></span>A-Z</a></li>
 				<li><a href="productos.php?order=ZA&marca=<?php echo (isset($_GET['marca'])?$_GET['marca']:'') ?>&cat=<?php echo isset($_GET['cat'])?$_GET['cat']:''?>"><span class="icon-chevron-right"></span>Z-A</a></li>
 				<li><a href="productos.php?order=&marca=<?php echo (isset($_GET['marca'])?$_GET['marca']:'') ?>&cat=<?php echo isset($_GET['cat'])?$_GET['cat']:''?>"><span class="icon-chevron-right"></span>Destacados</a></li>
+				-->
 				
+                <span>Ordenado Por</span>
+                <select onchange="ordenamiento()" id="order">
+                    <option value="AZ">A-Z</option>
+                    <option value="ZA">Z-A</option>
+					<option value="DES">Destacados</option>
+                </select>
+				<script>
+					function ordenamiento(a) {
+						var a = document.getElementById('order');
+						document.location.href = "productos.php?order="+(a.options[a.selectedIndex].value)+"&marca=<?php echo (isset($_GET['marca'])?$_GET['marca']:'') ?>&cat=<?php echo isset($_GET['cat'])?$_GET['cat']:''?>";
+						
+					}
+				</script>
 				
-			</ul>
+            </div>
 		</div>
 	</div>
 

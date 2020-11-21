41aec38fc495566c3cc734de2c1aba0771275e34
Lucas Basualdo <basualdo1995@gmail.com>
2020-11-21 19:20:56 -0300

    Se modifica partes/menu_filtrado para q acepte subcategorias

--- partes/menu_de_filtrado.php
+++ partes/menu_de_filtrado.php
@@ -1,31 +1,45 @@
 <div id="sidebar" class="span3">
 
-	<!-- categorias -->
-    <div class="well well-small">
-		<h4>Categorías</h4>
-		<ul class="nav flex-column">
-			<?php
-				require_once('clases/categorias.php');
-				$Cat = new Categorias($con);
-
-				foreach($Cat->getCategorias() as $cat){ 
-			?>
-				<li>
-					<a href="productos.php?cat=<?php echo $cat['id']?>&marca=<?php echo (isset($_GET['marca'])?$_GET['marca']:'') ?>&order=<?php echo isset($_GET['order'])?$_GET['order']:''?>">
-						<span class="icon-chevron-right"></span><?php echo $cat['nombre']?>
-					</a>
-				</li>
-			<?php }?>
-				<li>
-					<a href="productos.php?cat=&marca=<?php echo isset($_GET['marca'])?$_GET['marca']:''?>&order=<?php echo isset($_GET['order'])?$_GET['order']:''?>">
-						<span class="icon-chevron-right"></span>Todos
-					</a>
-				</li>
-		</ul>
-    </div>
-
 	<!--FUNCION PARA SUBCATEGORIAS FALTA APLICAR-->
-	
+
+								
+							<?php
+						require_once('db_con.php');
+
+						$con = new PDO('mysql:host='.$hostname.';dbname='.$database.';port='.$puerto, $username, $password);
+
+						$query = "SELECT * FROM categorias WHERE padre_id = 0";
+
+						$cate = $con->query($query);
+						
+						?>
+						<ul>
+							<?php foreach($cate as $cat){?>
+								<li>
+									<a href="productos.php?pcat=<?php echo $cat['id']?>" >
+										<?php echo $cat['nombre']?>
+									</a>
+									<?php 
+									$query = " SELECT * FROM categorias WHERE padre_id = ".$cat['id']; 
+									$sub = $con->query($query); ?>
+										<ul>
+										<?php foreach($sub as $scat){?>
+												<li>
+													<a href="productos.php?cat=<?php echo $scat['id']?>" >
+														<?php echo $scat['nombre']?>
+													</a>
+													
+												</li>
+											<?php } ?>
+										</ul> 
+								</li>
+							<?php } ?>
+						</ul>
+
+						<a href="productos.php?cat=&marca=<?php echo isset($_GET['marca'])?$_GET['marca']:''?>&order=<?php echo isset($_GET['order'])?$_GET['order']:''?>">
+						<span class="icon-chevron-right"></span>Todos
+
+
 	<?php
 	/*
 		define("LIMITE", 2);
